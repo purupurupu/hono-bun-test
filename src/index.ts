d452216c1d9673a1d8ca8feb696b0e259af38f19
purupurupu <purupurupu0prog@gmail.com>
2024-02-06 23:48:44 +0900

    chore:add prettier

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import { Hono } from "hono";
-import { logger } from "hono/logger";
-import { router as myRouter } from "./routes/router"; // router.tsからルーターをインポート
+import { Hono } from 'hono';
+import { logger } from 'hono/logger';
+import { router as myRouter } from './routes/router'; // router.tsからルーターをインポート
 
 const app = new Hono();
 
-app.use("*", logger());
+app.use('*', logger());
 
-app.route("/api", myRouter);
+app.route('/api', myRouter);
 
-app.get("/", (c) => c.text("Hello from the app!"));
+app.get('/', (c) => c.text('Hello from the app!'));
 
 export default app;
